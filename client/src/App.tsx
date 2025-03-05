ea4de7330883ee1a1f76a5b8d24a83bd24b4b930
Nathan Green <ngreen1632@gmail.com>
2025-03-05 11:33:36 -0600

    Refactor App component to integrate Apollo and React Router
    Replaced the previous layout structure with ApolloProvider and React Router for better state management and navigation. Added routes for SearchBooks, SavedBooks, LoginForm, and SignupForm. This sets up the app for a more scalable architecture.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,26 @@
-import './App.css';
-import { Outlet } from 'react-router-dom';
-
-import Navbar from './components/Navbar';
+import { ApolloProvider } from "@apollo/client";
+import client from "./utils/apolloClient";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import SearchBooks from "./pages/SearchBooks";
+import SavedBooks from "./pages/SavedBooks";
+import LoginForm from "./components/LoginForm";
+import SignupForm from "./components/SignupForm";
+import "./App.css";
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Outlet />
-    </>
+    <ApolloProvider client={client}>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<SearchBooks />} />
+          <Route path="/saved" element={<SavedBooks />} />
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/signup" element={<SignupForm />} />
+        </Routes>
+      </Router>
+    </ApolloProvider>
   );
 }
 
