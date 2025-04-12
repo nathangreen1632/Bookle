46f5ccdb00df230fd0fe00fab6203239afc375fb
Nathan Green <ngreen1632@gmail.com>
2025-04-12 14:15:33 -0500

    Refactor apolloClient.ts by removing redundant comments
    Cleaned up unnecessary comments in the Apollo Client setup to improve code readability. This streamlines the file while retaining all essential functionality and context.

--- client/src/utils/apolloClient.ts
+++ client/src/utils/apolloClient.ts
@@ -1,12 +1,11 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
 
-// Create HTTP link to backend GraphQL server
+
 const httpLink = createHttpLink({
   uri: "/graphql",
 });
 
-// Set up auth headers (automatically adds token if logged in)
 const authLink = setContext((_, { headers }) => {
   const token = localStorage.getItem("id_token");
   return {
@@ -17,7 +16,6 @@ const authLink = setContext((_, { headers }) => {
   };
 });
 
-// Create Apollo Client instance
 const client = new ApolloClient({
   link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
